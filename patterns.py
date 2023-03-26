7d70c9a7cea1c40adbc4d60fbfc9142a243aa6ba
i-priyansh-srivastava <113783062+i-priyansh-srivastava@users.noreply.github.com>
2023-03-27 00:06:20 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -111,3 +111,64 @@ for k in range(p+1):
 '''
 
 
+'''
+#    *
+#   ***
+#  *****
+#   ***
+#    *
+
+n=int(input("enter number of rows"))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end="")
+    for l in range(i):
+        print("*",end="")
+    print()
+for i in range(n):
+    for k in range(i+1):
+        print(" ",end="")
+    for j in range(n-i-1):
+        print("*",end="")
+    for k in range(n-i-2):
+        print("*",end="")
+    
+    print() 
+'''
+
+
+'''
+# *
+# **
+# * *
+# *  * 
+# *****
+n=int(input("enter the no. of rows "))
+for i in range(n):
+    if i==0 or i==n-1:
+        print("*"*(i+1))
+    else:
+        print("*"," "*(i-1),"*",sep="")
+'''
+
+
+'''
+# *      * 
+# **    **
+# ***  ***
+# ********
+
+
+# n=int(input("enter number of rows"))
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end="")
+#     for k in range(n-i-1):
+#         print(" "*2,end="")
+#     for l in range(i+1):
+#         print("*",end="")
+    
+#     print()
+'''
